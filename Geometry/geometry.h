63d52b5d8cb7d0cfcb35e5248e2713ad4a2d6ed3
milan-koren <milan.koren3@gmail.com>
2022-02-23 14:53:47 +0100

    Create geometry.h

---
+++ Geometry/geometry.h
@@ -0,0 +1,30 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+/*!
+ * *****************************************************************
+ *                             G3DTCore
+ * *****************************************************************
+ * \file geometry.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https://github.com/milan-koren/G3DTCore
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "box2d.h"
+#include "box3dt.h"
+#include "index2d.h"
+#include "index3d.h"
+#include "index3dt.h"
+#include "point2d.h"
+#include "point3d.h"
+#include "point3dt.h"
+#include "rasterblock.h"
+#include "rastersize2d.h"
+#include "rastersize3d.h"
+#include "rastersize3dt.h"
+
+#endif // GEOMETRY_H
