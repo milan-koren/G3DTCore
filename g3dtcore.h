d00f068ede3a908477f839b6ea454c53356f51ac
milan-koren <milan.koren3@gmail.com>
2022-02-23 14:52:30 +0100

    Create g3dtcore.h

---
+++ g3dtcore.h
@@ -0,0 +1,21 @@
+#ifndef G3DTCORE_H
+#define G3DTCORE_H
+
+/*!
+ * *****************************************************************
+ *                             G3DTCore
+ * *****************************************************************
+ * \file g3dtcore.h
+ *
+ * \author M. Koren, milan.koren3@gmail.com
+ * Source: https://github.com/milan-koren/G3DTCore
+ * Licence: EUPL v. 1.2
+ * https://joinup.ec.europa.eu/collection/eupl
+ * *****************************************************************
+ */
+
+#include "g3dtcore_global.h"
+#include "Geometry/geometry.h"
+#include "g3dtworker.h"
+
+#endif // G3DTCORE_H
